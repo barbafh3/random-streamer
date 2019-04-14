1df9c6339e253d8d8dfb9c2c0262f81a0846538e
barbafh <barbafh@gmail.com>
2019-04-14 11:22:27 -0300

    Added ReduxForm basic functionality

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { signIn, signOut } from '../actions/streamActions';
 
 class GoogleAuth extends Component{
-
-    state = {
-        isSignedIn: null
-    };
 
     componentWillMount(){
         window.gapi.load('client:auth2', async () => {
@@ -14,36 +13,40 @@ class GoogleAuth extends Component{
                 scope: 'email'
             });
             this.auth = window.gapi.auth2.getAuthInstance(); 
-            this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+            this.onAuthChange(this.auth.isSignedIn.get());
             this.auth.isSignedIn.listen(this.onAuthChange);
         });
     }
 
-    onAuthChange = () => {
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    onAuthChange = isSignedIn => {
+        if (isSignedIn) {
+            this.props.signIn(this.auth.currentUser.get().getId());
+        } else {
+            this.props.signOut();
+        }
     };
 
-    trySignIn = () => {
+    onSignInClick = () => {
         this.auth.signIn();
     }
 
-    trySignOut = () => {
+    onSignOutClick = () => {
         this.auth.signOut();
     }
 
     renderAuthButton(){
-        if (this.state.isSignedIn === null) {
+        if (this.props.isSignedIn === null) {
             return null;
-        } else if (this.state.isSignedIn) {
+        } else if (this.props.isSignedIn) {
             return (
-                <button className='ui red google button' onClick={ this.trySignOut }>
+                <button className='ui red google button' onClick={ this.onSignOutClick }>
                     <i className='google icon' />
                     Sign Out
                 </button>
             );
         } else {
             return (
-                <button className='ui red google button' onClick={ this.trySignIn }>
+                <button className='ui red google button' onClick={ this.onSignInClick }>
                     <i className='google icon' />
                     Sign In with Google
                 </button>
@@ -59,4 +62,8 @@ class GoogleAuth extends Component{
 
 }
 
-export default GoogleAuth;
+const mapStateToProps = state => {
+    return { isSignedIn: state.auth.isSignedIn };
+}
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
-};
+class StreamCreate extends Component{
 
-export default StreamCreate;
+    // input destructured from formProps argument
+    renderInput({ input, label }) {
+        return (
+            <div className='field'>
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        );
+    }
+
+    onSubmit(formValues) {
+        
+    }
+
+    render(){
+        return (
+            <div>
+                <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form'>
+                    <Field name='title' component={ this.renderInput } label='Enter Title' />
+                    <Field name='description' component={ this.renderInput } label='Enter Description' />
+                    <button className='ui button primary'>Submit</button>
+                </form>
+            </div>
+        );
+    };
+
+}
+
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
